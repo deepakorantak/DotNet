3a5b2ea30f71ed67084250ba509b074ef95de128
Deepa Korantak <deepakorantak@gmail.com>
2016-06-22 16:20:04 +0530

    Final version

---
+++ PropertiesVsFields/PropertiesVsFields/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesVsFields
+{
+    class Person
+    {
+        public string Name
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+            set
+            {
+                if(!string.IsNullOrEmpty(value) && value.IndexOf(" ")>0)
+                {
+                    firstName = value.Substring(0,value.IndexOf(" ")).Trim();
+                    lastName = value.Substring(value.IndexOf(" ")+1).Trim();
+
+                }
+            }
+        }    
+        private string firstName,lastName;
+        
+        public Person(string ifirstName,string ilastName)
+        {
+            firstName = ifirstName;
+            lastName = ilastName;
+
+        }
+    }
+}
---
+++ PropertiesVsFields/PropertiesVsFields/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesVsFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Deepa", "Korantak");
+            Console.WriteLine("Full Name : " + p1.Name);
+
+            p1.Name = "Gauri Damle";
+            Console.WriteLine("Full Name : " + p1.Name);
+
+            p1.Name = "Sachin  Korantak";
+            Console.WriteLine("Full Name : " + p1.Name);
+
+            p1.Name = null;
+            Console.WriteLine("Full Name : " + p1.Name);
+        }
+    }
+}
