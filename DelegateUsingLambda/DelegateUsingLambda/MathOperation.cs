7486d4a889b94959d350be2a7a4e696ea39bbf16
Deepa Korantak <deepakorantak@gmail.com>
2016-07-09 18:43:49 +0530

    FuncT and Delegate

---
+++ DelegateUsingLambda/DelegateUsingLambda/MathOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateUsingLambdaFuncT
+{
+    class MathOperation
+    {
+
+        public double MathProcess(double x,double y , Func<double,double,double> Operation)
+        {
+            double result = 0;
+            Console.WriteLine("Inside Mathprocess with double arguments"  );            
+            result = Operation(x, y);
+            return result;
+        }
+ 
+    }
+}
---
+++ DelegateUsingLambda/DelegateUsingLambda/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateUsingLambdaFuncT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 0, y = 0, result = 0;
+            Func<double, double, double> Addition = (x1, y1) => x1 + y1;
+            Func<double, double, double> Subtract = (x1, y1) => x1 - y1;
+            Func<double, double, double> Mutiply = (x1, y1) => x1 * y1;
+            Func<double, double, double> Division = (x1, y1) => x1 / y1;
+
+            MathOperation oper = new MathOperation();
+
+            x = 5.35;
+            y = 6.65;
+
+                        Console.WriteLine("Enter Add - addition, Sub - Subtraction,Mul - Multiplication, Div - Division");
+            string option = Console.ReadLine();
+
+            switch (option.ToUpper())
+            {
+
+                case "ADD":
+                    result = oper.MathProcess(x, y, Addition);
+                    break;
+                case "SUB":
+                    result = oper.MathProcess(x, y, Subtract);
+                    break;
+                case "MUL":
+                    result = oper.MathProcess(x, y, Mutiply);
+                    break;
+                case "DIV":
+                    result = oper.MathProcess(x, y, Division);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"Result of operation {option.ToUpper()} {x},{y} is {result}");
+        }
+    }
+}
