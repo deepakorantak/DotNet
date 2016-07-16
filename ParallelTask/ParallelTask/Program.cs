24a45dc73d1aa92dd2c0f33d658a2d49c0ea9dab
Deepa Korantak <deepakorantak@gmail.com>
2016-07-16 22:26:51 +0530

    Parallel Tasks Example

---
+++ ParallelTask/ParallelTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelTask
+{
+    class Program
+    {
+        static string[] apps = { "Excel", "Notepad", "Calc" };
+
+        static void Main(string[] args)
+        {
+            AsyncProcessing();
+            Console.WriteLine($"Asyn Processing done in Main function thread {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static void AsyncProcessing()
+        {           
+            Action<string> _app = PerformTask;
+            Parallel.ForEach(apps, _app);             
+        }
+
+        static void PerformTask(object _app)
+        {
+            Process myProcess = new Process();
+            myProcess.StartInfo.FileName = _app.ToString();
+            myProcess.Start();
+            Console.WriteLine($"Application - {_app.ToString()} opened on thread - {Thread.CurrentThread.ManagedThreadId} at {DateTime.Now}");
+            Thread.Sleep(10000);
+            myProcess.CloseMainWindow();
+            Console.WriteLine($"Application - {_app.ToString()} closed on thread - {Thread.CurrentThread.ManagedThreadId} at {DateTime.Now}");
+            
+        }
+    }
+}
---
+++ ParallelTask/ParallelTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ParallelTask")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CTS")]
+[assembly: AssemblyProduct("ParallelTask")]
+[assembly: AssemblyCopyright("Copyright © CTS 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f49befb-e8a5-42fb-b6b2-49769e6c2f02")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
