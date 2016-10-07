7918fe0ed8f20d98e10f0dd707c28c8598a79863
Deepa Korantak <deepa.korantak@cognizant.com>
2016-10-08 01:40:17 +0530

    MS Cognitive Service - OCR

---
+++ MSVisionAPI/MSVisionAPI/VisionAPIFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System.IO;
+
+namespace MSVisionAPI
+{
+    public class VisionAPIFunctions
+    {
+
+        public static async Task<OcrResults> UploadAndRecognizeImage(string imageFilePath, string language,string SubscriptionKey)
+        {
+
+            Console.WriteLine( $"Image File : {imageFilePath}");
+            Console.WriteLine($"Langauage : {language}");
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey);
+            Console.WriteLine("VisionServiceClient is created");
+
+            using (Stream imageFileStream = File.OpenRead(imageFilePath))
+            {
+               
+                Console.WriteLine("Calling VisionServiceClient.RecognizeTextAsync()...");
+                OcrResults ocrResult = await VisionServiceClient.RecognizeTextAsync(imageFileStream, language);
+                return ocrResult;
+            }
+
+             
+        }
+
+
+    
+
+        
+     
+
+    }
+}
---
+++ TestMSVisionAPI/TestMSVisionAPI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSVisionAPI;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace TestMSVisionAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dowork();
+            Console.ReadLine();
+        }
+
+        public static async Task Dowork()
+        {
+
+            string ocrImageFile = "D:\\Deepa\\OCR.JPG";
+            string ocrImageLang = "unk";
+            string visionAPISubKey = "cd708778b49f4d3e8849f9cae530a928";
+            OcrResults res =  await VisionAPIFunctions.UploadAndRecognizeImage(ocrImageFile, ocrImageLang, visionAPISubKey);
+            LogOcrResults(res);
+        }
+
+
+        protected static void LogOcrResults(OcrResults results)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (results != null && results.Regions != null)
+            {
+                stringBuilder.Append("Text: ");
+                stringBuilder.AppendLine();
+                foreach (var item in results.Regions)
+                {
+                    foreach (var line in item.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            stringBuilder.Append(word.Text);
+                            stringBuilder.Append(" ");
+                        }
+
+                        stringBuilder.AppendLine();
+                    }
+
+                    stringBuilder.AppendLine();
+                }
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+
+    }
+}
