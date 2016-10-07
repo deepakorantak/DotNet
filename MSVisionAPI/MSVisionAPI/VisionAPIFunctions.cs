0e714f48fa9b22ade45b2167b434160da8a726ce
Deepa Korantak <deepa.korantak@cognizant.com>
2016-10-08 02:13:39 +0530

    code snippet for MS cognitive Vision - OCR API

--- MSVisionAPI/MSVisionAPI/VisionAPIFunctions.cs
+++ MSVisionAPI/MSVisionAPI/VisionAPIFunctions.cs
@@ -25,17 +25,54 @@ namespace MSVisionAPI
                
                 Console.WriteLine("Calling VisionServiceClient.RecognizeTextAsync()...");
                 OcrResults ocrResult = await VisionServiceClient.RecognizeTextAsync(imageFileStream, language);
+                LogOcrResults(ocrResult);
                 return ocrResult;
             }
 
              
         }
 
+        public static string Dowork(string ocrImageFile,string ocrImageLang,string visionAPISubKey)
+        {
 
-    
+            //string ocrImageFile = "D:\\Deepa\\OCR.JPG";
+            //string ocrImageLang = "unk";
+            //string visionAPISubKey = "cd708778b49f4d3e8849f9cae530a928";
+            Task<OcrResults> res = UploadAndRecognizeImage(ocrImageFile, ocrImageLang, visionAPISubKey);
+            return LogOcrResults(res.Result);
+        }
 
-        
-     
+        protected static string LogOcrResults(OcrResults results)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (results != null && results.Regions != null)
+            {
+                stringBuilder.Append("Text: ");
+                stringBuilder.AppendLine();
+                foreach (var item in results.Regions)
+                {
+                    foreach (var line in item.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            stringBuilder.Append(word.Text);
+                            stringBuilder.Append(" ");
+                        }
+
+                        stringBuilder.AppendLine();
+                    }
+
+                    stringBuilder.AppendLine();
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+
+
+
 
     }
 }
--- TestMSVisionAPI/TestMSVisionAPI/Program.cs
+++ TestMSVisionAPI/TestMSVisionAPI/Program.cs
@@ -13,49 +13,14 @@ namespace TestMSVisionAPI
         static void Main(string[] args)
         {
 
-            Dowork();
-            Console.ReadLine();
-        }
-
-        public static async Task Dowork()
-        {
-
             string ocrImageFile = "D:\\Deepa\\OCR.JPG";
             string ocrImageLang = "unk";
             string visionAPISubKey = "cd708778b49f4d3e8849f9cae530a928";
-            OcrResults res =  await VisionAPIFunctions.UploadAndRecognizeImage(ocrImageFile, ocrImageLang, visionAPISubKey);
-            LogOcrResults(res);
+            string resultText = VisionAPIFunctions.Dowork(ocrImageFile, ocrImageLang, visionAPISubKey);
+            Console.WriteLine(resultText); 
+            Console.ReadLine();
         }
 
-
-        protected static void LogOcrResults(OcrResults results)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-
-            if (results != null && results.Regions != null)
-            {
-                stringBuilder.Append("Text: ");
-                stringBuilder.AppendLine();
-                foreach (var item in results.Regions)
-                {
-                    foreach (var line in item.Lines)
-                    {
-                        foreach (var word in line.Words)
-                        {
-                            stringBuilder.Append(word.Text);
-                            stringBuilder.Append(" ");
-                        }
-
-                        stringBuilder.AppendLine();
-                    }
-
-                    stringBuilder.AppendLine();
-                }
-            }
-
-            Console.WriteLine(stringBuilder.ToString());
-        }
-
-
+      
     }
 }
