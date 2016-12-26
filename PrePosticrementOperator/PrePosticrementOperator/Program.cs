a370f22f6d02da400a7505f15754311d42e747a9
Deepa Korantak <deepa.korantak@cognizant.com>
2016-12-26 15:28:44 +0530

    post and pre ++ operator example

---
+++ PrePosticrementOperator/PrePosticrementOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrePosticrementOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            int res;
+
+            res = i++;
+            Console.WriteLine($" Postfix ++  result : {res} and i : {i}  ");
+
+            res = ++i;
+            Console.WriteLine($" Prefix ++ result : {res} and i : {i}  ");
+        }
+    }
+}
