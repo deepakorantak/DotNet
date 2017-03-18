d0138560b5a6ed14c5d25437092dc69b5c7a15da
Deepa Korantak <deepa.korantak@cognizant.com>
2017-03-19 03:08:25 +0530

    Formatting done

--- DAFTriggerUtility/DAFTriggerUtility/Program.cs
+++ DAFTriggerUtility/DAFTriggerUtility/Program.cs
@@ -17,33 +17,31 @@ namespace DAFTriggerUtility
             {
                 foreach (var trig in item.ListTriggerType.Values)
                 {
-                    trig.TriggerCreateSyntax = trig.TriggerCreateSyntax.Replace("{table}", item.TableName) ;
+                    var triCreate = trig.TriggerCreateSyntax.Replace("{table}", item.TableName) + "\n" ;
                     //Console.WriteLine(trig.TriggerCreateSyntax);
 
-                    var tribody =   "\n" + trig.TriggerBodyCondition + "\n" ;
+                    var tricondition = "\n\t"+ trig.TriggerBodyCondition + "\n" ;
 
 
                     if (trig.IsHistorytable == "Yes")
                     {
-                        var hist_column_list = item.HistoryColumnList.Aggregate((a,c) => string.Join(",\n",a,c));
+                        var hist_column_list = item.HistoryColumnList.Aggregate((a,c) => string.Join(",\n\t",a,c));
                         //Console.WriteLine(hist_column_list);
 
-                        var column_list = "OLD." +  item.ColumnList.Aggregate((a, c) => string.Join(",\n",a, "OLD." + c));
+                        var column_list = "NULL,\n\t'update',\n\tOLD." +  item.ColumnList.Aggregate((a, c) => string.Join(",\n\t",a, "OLD." + c));
                       //  Console.WriteLine(column_list);
 
-                        trig.HistoryTableInsert =  trig.HistoryTableInsert.Replace("{hist_table}", item.HistoryTableName);
-                        trig.HistoryTableInsert =  trig.HistoryTableInsert.Replace("{column_list}", hist_column_list);
-                        trig.HistoryTableInsert =  trig.HistoryTableInsert.Replace("{value_list}", column_list);
+                        var trihistory = trig.HistoryTableInsert.Replace("{hist_table}", item.HistoryTableName);
+                        trihistory = trihistory.Replace("{column_list}", hist_column_list);
+                        trihistory = trihistory.Replace("{value_list}", column_list);
 
                         //Console.WriteLine(trig.HistoryTableInsert);
-                        tribody = tribody + "\n" + trig.HistoryTableInsert + "\n";
-
-
+                        tricondition = tricondition + "\n" + trihistory + "\n";
                     }
 
-                    trig.TriggerBodySyntax = trig.TriggerBodySyntax.Replace("{body}", tribody);
+                    var tribody= trig.TriggerBodySyntax.Replace("{body}", tricondition);
 
-                    var finalTriggersyntax = trig.TriggerCreateSyntax + "\n\n" +  trig.TriggerBodySyntax;
+                    var finalTriggersyntax = triCreate + "\n\n" + tribody;
                     Console.WriteLine(finalTriggersyntax);
 
                 }
