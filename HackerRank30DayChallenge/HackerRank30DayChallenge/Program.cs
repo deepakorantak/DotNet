5a5756b331576f021d55453c400e2655a913566d
Deepa Korantak <deepa.korantak@cognizant.com>
2017-03-23 16:49:46 +0530

    Challenge Day 7

--- HackerRank30DayChallenge/HackerRank30DayChallenge/Program.cs
+++ HackerRank30DayChallenge/HackerRank30DayChallenge/Program.cs
@@ -10,7 +10,71 @@ namespace HackerRank30DayChallenge
     {
         static void Main(string[] args)
         {
-            ThirtyDayChallengeDay4();
+            //ThirtyDayChallengeDay4();
+            //ThirtyDayChallengeDay6();
+            ThirtyDayChallengeDay7();
+        }
+
+        private static void ThirtyDayChallengeDay7()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 1 && n <= 1000)
+            {
+                string[] arr_temp = Console.ReadLine().Split(' ');
+                int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+                var notvalid = arr.Any(a => (a < 1 || a > 10000));
+                if (!notvalid)
+                {
+                    var output = arr.Reverse()
+                       .Select(a => a.ToString())
+                       .Aggregate((a, c) => a + " " + c)
+                       .TrimEnd()
+                       ;
+
+                    Console.WriteLine(output);
+                }
+            }
+        }
+
+        private static void ThirtyDayChallengeDay6()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            if (n >= 1 && n <= 10)
+            {
+
+                string S = "";
+
+                for (int i = 1; i <= n; i++)
+                {
+                    S = Console.ReadLine();
+                    if (S.Length >= 1 && S.Length <= 10000)
+                    {
+
+                        string evenS = "", oddS = "";
+                        for (int j = 0; j < S.Length; j++)
+                        {
+
+                            int remainder;
+                        int result  = Math.DivRem(j, 2, out remainder);
+                            if (remainder == 0)
+                            {
+
+                                evenS += S[j];
+
+                            }
+                            else
+                            {
+                                oddS+= S[j];
+                            }
+
+                        }
+
+                        Console.WriteLine($"{evenS} {oddS}");
+                   
+
+                    }
+                }
+            }
         }
 
         private static void ThirtyDayChallengeDay4()
