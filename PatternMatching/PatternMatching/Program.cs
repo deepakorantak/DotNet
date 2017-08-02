a8166532bad1b774d0937977afee7d6fc4ee130c
Deepa Korantak <deepa.korantak@cognizant.com>
2017-08-02 23:26:05 +0200

    Derived CODA description as well

--- PatternMatching/PatternMatching/Program.cs
+++ PatternMatching/PatternMatching/Program.cs
@@ -8,8 +8,8 @@ namespace PatternMatching
 {
     public class Program
     {
-       static List<Transaction> sourceTrans;
-       static List<Pattern> patternConfig;
+        static List<Transaction> sourceTrans;
+        static List<Pattern> patternConfig;
         static void Main(string[] args)
         {
             sourceTrans = ProcessTransactionList.GetTransactions("D:\\Deepa\\Projects\\VGZ\\Robot\\output\\optransactions.csv");
@@ -18,13 +18,8 @@ namespace PatternMatching
             Process();
 
             Console.WriteLine("Completed");
-
-
-
-
-
         }
-        
+
         static void Process()
         {
 
@@ -33,8 +28,8 @@ namespace PatternMatching
                                                   .SelectMany(p => { return MatchingBasicPattern(p); })
                                                   .ToList();
 
-            var notmatchedList = sourceTrans.Except(matchedList,new TransactionComparer()).ToList();
-            sourceTrans =  notmatchedList.Union(matchedList).ToList();
+            var notmatchedList = sourceTrans.Except(matchedList, new TransactionComparer()).ToList();
+            sourceTrans = notmatchedList.Union(matchedList).ToList();
 
             matchedList = patternConfig.Where(p => p.isAdditionPattern == "Add")
                                       .SelectMany(p => { return MatchingAdditionPattern(p); })
@@ -53,10 +48,10 @@ namespace PatternMatching
             using (var file = File.CreateText("D:\\Deepa\\Projects\\VGZ\\Robot\\output\\maptransactions.csv"))
             {
 
-                file.WriteLine("id|load_date|iban|companycode|statement_numbe|entry_date|value_date|glaccount|amount|remarks|additional");
-                sourceTrans.ForEach(t =>  file.WriteLine(string.Join("|", new[] {t.id,t.laaddatum,t.iban,t.companyCode,t.statementNumber,t.entryDate,
-                                                                                 t.valueDate,t.glAccountNumber,t.amount,t.omschrijving,t.naamTegenpartij })));
-              
+                file.WriteLine("id|load_date|iban|companycode|statement_numbe|entry_date|value_date|glaccount|amount|codadesc|remarks|additional");
+                sourceTrans.ForEach(t => file.WriteLine(string.Join("|", new[] {t.id,t.laaddatum,t.iban,t.companyCode,t.statementNumber,t.entryDate,
+                                                                                 t.valueDate,t.glAccountNumber,t.amount,t.codaDescription,t.omschrijving,t.naamTegenpartij })));
+
             }
 
             Console.WriteLine("Process completed");
@@ -66,25 +61,26 @@ namespace PatternMatching
         private static IEnumerable<Transaction> MatchingBasicPattern(Pattern p)
         {
             var regex = new Regex(p.mainPattern);
-            return  sourceTrans.Where(s => regex.IsMatch(s.omschrijving.ToLower()))
+            return sourceTrans.Where(s => regex.IsMatch(s.omschrijving.ToLower()))
                                .Select(s => new Transaction
-                                            {
-                                                id = s.id,
-                                                laaddatum = s.laaddatum,
-                                                iban = s.iban,
-                                                statementNumber = s.statementNumber,
-                                                entryDate = s.entryDate,
-                                                valueDate = s.valueDate,
-                                                omschrijving = s.omschrijving,
-                                                amount = s.amount,
-                                                naamTegenpartij = s.naamTegenpartij,
-                                                glAccountNumber = p.glAccountNumber,
-                                                companyCode =s.companyCode
-                                            }
+                               {
+                                   id = s.id,
+                                   laaddatum = s.laaddatum,
+                                   iban = s.iban,
+                                   statementNumber = s.statementNumber,
+                                   entryDate = s.entryDate,
+                                   valueDate = s.valueDate,
+                                   omschrijving = s.omschrijving,
+                                   amount = s.amount,
+                                   naamTegenpartij = s.naamTegenpartij,
+                                   glAccountNumber = p.glAccountNumber,
+                                   companyCode = s.companyCode,
+                                   codaDescription = GetCODADescription(p.codaDescription, s.omschrijving)
+
+                               }
                                 );
-
         }
-
+ 
         private static IEnumerable<Transaction> MatchingAdditionPattern(Pattern p)
         {
             var regexMain = new Regex(p.mainPattern);
@@ -94,18 +90,19 @@ namespace PatternMatching
                                           s.glAccountNumber == "NA"
                                     )
                               .Select(s => new Transaction
-                                                {
-                                                    id = s.id,
-                                                    laaddatum = s.laaddatum,
-                                                    iban = s.iban,
-                                                    statementNumber = s.statementNumber,
-                                                    entryDate = s.entryDate,
-                                                    valueDate = s.valueDate,
-                                                    omschrijving = s.omschrijving,
-                                                    amount = s.amount,
-                                                    naamTegenpartij = s.naamTegenpartij,
-                                                    glAccountNumber = p.glAccountNumber,
-                                                    companyCode = s.companyCode
+                              {
+                                  id = s.id,
+                                  laaddatum = s.laaddatum,
+                                  iban = s.iban,
+                                  statementNumber = s.statementNumber,
+                                  entryDate = s.entryDate,
+                                  valueDate = s.valueDate,
+                                  omschrijving = s.omschrijving,
+                                  amount = s.amount,
+                                  naamTegenpartij = s.naamTegenpartij,
+                                  glAccountNumber = p.glAccountNumber,
+                                  companyCode = s.companyCode,
+                                  codaDescription = GetCODADescription(p.codaDescription, s.omschrijving)
                               }
                                     );
         }
@@ -113,34 +110,55 @@ namespace PatternMatching
         private static IEnumerable<Transaction> MatchingAFRPattern(Pattern p)
         {
             var regexMain = new Regex(p.mainPattern);
-           
+
             return sourceTrans.Where(s => regexMain.IsMatch(s.omschrijving.ToLower()))
                               .Select(s => new Transaction
-                                                {
-                                                    id = s.id,
-                                                    laaddatum = s.laaddatum,
-                                                    iban = s.iban,
-                                                    statementNumber = s.statementNumber,
-                                                    entryDate = s.entryDate,
-                                                    valueDate = s.valueDate,
-                                                    omschrijving = s.omschrijving,
-                                                    amount = s.amount,
-                                                    naamTegenpartij = s.naamTegenpartij,
-                                                    glAccountNumber = GetAccounts(s.omschrijving, " " + s.companyCode),
-                                                    companyCode = s.companyCode
-                                                }
+                              {
+                                  id = s.id,
+                                  laaddatum = s.laaddatum,
+                                  iban = s.iban,
+                                  statementNumber = s.statementNumber,
+                                  entryDate = s.entryDate,
+                                  valueDate = s.valueDate,
+                                  omschrijving = s.omschrijving,
+                                  amount = s.amount,
+                                  naamTegenpartij = s.naamTegenpartij,
+                                  glAccountNumber = GetAccounts(s.omschrijving, " " + s.companyCode),
+                                  companyCode = s.companyCode,
+                                  codaDescription = GetCODADescription(p.codaDescription, s.omschrijving)
+                              }
                                     );
         }
 
         private static string GetAccounts(string inputString, string matchingCode)
         {
-           
+
             var startindex = inputString.IndexOf(matchingCode, 0) + matchingCode.Count();
             var endindex = inputString.IndexOf(" ", startindex + 2);
             var numberChars = ((endindex - startindex) < 0) ? inputString.Length - startindex : endindex - startindex;
             var res = inputString.Substring(startindex, numberChars).Trim().Replace(":", "");
 
             return res;
+        }
+
+        private static string GetCODADescription(string config, string omschrijving)
+        {
+
+            if (config.Contains("OMSCHRIJVING"))
+            {
+                var s = config.Split('|');
+                var numChars = Int32.Parse(s[1].Replace("]",""));
+                var length = ( numChars > omschrijving.Length) ? omschrijving.Length : numChars ;
+                var res = omschrijving.Substring(0,length);
+                return res;
+            };
+
+            if (config.Contains("[MM YYYY]"))
+            {
+                return config.Replace("[MM YYYY]", DateTime.Now.Month.ToString() + " " + DateTime.Now.Year.ToString());
+            };
+
+            return config;
         }
     }
 }
--- PatternMatching/PatternMatching/Source.cs
+++ PatternMatching/PatternMatching/Source.cs
@@ -21,6 +21,7 @@ namespace PatternMatching
         public string amount { get; set; }
         public string glAccountNumber { get; set; }
         public string naamTegenpartij { get; set; }
+        public string codaDescription { get; set; }
     }
 
     public class TransactionComparer : IEqualityComparer<Transaction>
@@ -55,13 +56,14 @@ namespace PatternMatching
                 id = dr["ID"].ToString(),
                 laaddatum = dr["LAADDATUM"].ToString(),
                 iban = dr["IBAN"].ToString(),
+                companyCode = listofAccountMappings.Where(s => s.iban == dr["IBAN"].ToString()).Select(s => s.companyCode).First(),
                 statementNumber = dr["STATEMENT_NUMBER"].ToString(),
                 entryDate = dr["ENTRY_DATE"].ToString(),
                 valueDate = dr["VALUE_DATE"].ToString(),
                 omschrijving = dr["OMSCHRIJVING"].ToString(),
                 amount = dr["AMOUNT"].ToString(),
                 naamTegenpartij = dr["NAAM_TEGENPARTIJ"].ToString(),
-                companyCode = listofAccountMappings.Where(s=> s.iban == dr["IBAN"].ToString()).Select(s=>s.companyCode).First()
+                codaDescription = ""
 
             };
         }
@@ -79,6 +81,7 @@ namespace PatternMatching
                                     id = str[0],
                                     laaddatum = str[1],
                                     iban = str[2],
+                                    companyCode = listofAccountMappings.Where(a => a.iban == str[2]).Select(a => a.companyCode).First(),
                                     statementNumber = str[3],
                                     entryDate = str[4],
                                     valueDate = str[5],
@@ -86,7 +89,8 @@ namespace PatternMatching
                                     amount = str[7],
                                     naamTegenpartij = str[8],
                                     glAccountNumber = "NA",
-                                    companyCode = listofAccountMappings.Where(a => a.iban == str[2]).Select(a => a.companyCode).First()
+                                    codaDescription = ""
+                                    
                                 };
                             }
                        ).ToList();
